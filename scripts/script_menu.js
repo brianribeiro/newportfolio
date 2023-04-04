18916290de4489681e4b3deeac19579bd0eeb667
Brian Ribeiro <99487199+brianribeiro@users.noreply.github.com>
2023-04-04 03:36:51 -0300

    Atualização para divulgação
    Últimas alterações para o início da divulgação do meu novo portfólio.

--- scripts/script_menu.js
+++ scripts/script_menu.js
@@ -3,10 +3,10 @@ function toggleMenu() {
     var menuButton = document.querySelector('.menu_button')
     if (x.style.display === 'flex') {
         x.style.display = 'none'
-        document.querySelector('#button_status').src = './images/menu.png'
+        document.querySelector('#button_status').src = './images/icons/menu.png'
         
     } else {
         x.style.display = 'flex'
-        document.querySelector('#button_status').src = './images/close.png'
+        document.querySelector('#button_status').src = './images/icons/close.png'
     }
 }
